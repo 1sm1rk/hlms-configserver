c6d239e4bbc49696bce653fe3fe036d6a7c0a31d
sm1rk <smirk.mueller@gmail.com>
2023-02-06 22:01:00 +0100

    inital import

---
+++ src/main/java/de/homelabs/hlms/configserver/HlmsConfigserverApplication.java
@@ -0,0 +1,13 @@
+package de.homelabs.hlms.configserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HlmsConfigserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HlmsConfigserverApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/homelabs/hlms/configserver/HlmsConfigserverApplicationTests.java
@@ -0,0 +1,13 @@
+package de.homelabs.hlms.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HlmsConfigserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
