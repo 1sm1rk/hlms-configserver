4ddf7a586b457d5777c840a96b350b93a64060b4
sm1rk <smirk.mueller@gmail.com>
2023-06-18 13:52:21 +0200

    build preparations

--- src/main/java/de/homelabs/hlms/configserver/HlmsConfigserverApplication.java
+++ src/main/java/de/homelabs/hlms/configserver/HlmsConfigserverApplication.java
@@ -1,10 +1,16 @@
 package de.homelabs.hlms.configserver;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
+@EnableConfigServer
 @SpringBootApplication
 public class HlmsConfigserverApplication {
+
+	private static Logger logger = LoggerFactory.getLogger(HlmsConfigserverApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(HlmsConfigserverApplication.class, args);
